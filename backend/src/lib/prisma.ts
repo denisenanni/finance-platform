e5bbfac5a16d6f949c6027c041ce1a6ae83723ca
Denise Nanni <denise.nanni@tailoor.com>
2025-08-19 15:34:37 +0200

    feat: prisma client

---
+++ backend/src/lib/prisma.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "generated/prisma";
+
+declare global {
+  var __prisma: PrismaClient | undefined;
+}
+
+export const prisma = global.__prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  global.__prisma = prisma;
+}
